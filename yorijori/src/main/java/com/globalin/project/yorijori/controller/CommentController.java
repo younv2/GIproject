e837c3834546e05e08fc284e2ae2e5b99348da3d
YUN\younv <younv2@naver.com>
2023-12-14 10:06:12 +0900

    회원 정보 수정 작업

--- yorijori/src/main/java/com/globalin/project/yorijori/controller/CommentController.java
+++ yorijori/src/main/java/com/globalin/project/yorijori/controller/CommentController.java
@@ -1,11 +1,8 @@
 package com.globalin.project.yorijori.controller;
 
 import com.globalin.project.yorijori.service.impl.CommentService;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
